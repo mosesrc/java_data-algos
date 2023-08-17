54bb9c926101f30932353b22028203655c18df8d
mosesrc <ryanchristophermoses@gmail.com>
2023-08-17 10:29:16 -0400

    Selection Sort

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -13,6 +13,7 @@ public class Test {
     }
 
     public static void bubbleSort(int[] array) {
+        System.out.println("Bubble Sort: ");
         System.out.println(Arrays.toString(array) + "\n");
         for (int lastUnsortedIndex = array.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {  // 4
             for (int i = 0; i < lastUnsortedIndex; i++) {
@@ -20,14 +21,36 @@ public class Test {
                     swap(array, i, i + 1);
                 }
             }
-        String arrayPrinted = Arrays.toString(array);
-        System.out.println(arrayPrinted);
+            String arrayPrinted = Arrays.toString(array);
+            System.out.println(arrayPrinted);
         }
+        System.out.println("\n\n");
     }
-    
+
+    public static void selectionSort(int[] array) {
+        System.out.println("Selection Sort: ");
+        for (int lastUnsortedIndex = array.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            System.out.println("\nOuterLoop Start: " + Arrays.toString(array));
+            int largest = 0;
+
+            for (int i = 1; i <= lastUnsortedIndex; i++) {
+                System.out.println("\nLargest: " + array[largest] + "    Position: " + array[i] + "    Last Unsorted Index: " + array[lastUnsortedIndex]);
+                System.out.println("InnerLoop: " + Arrays.toString(array));
+                if (array[i] > array[largest]) {
+                    largest = i;
+                }
+            }
+            swap(array, largest, lastUnsortedIndex);
+            System.out.println("----------------------------" + "OuterLoop End: " + Arrays.toString(array));
+            System.out.println();
+        }
+        System.out.println("\n\n");
+    }
+
     public static void main(String[] args) {
         int[] intArray = {20, 35, -15, 7, 55, 1, -22};
-        bubbleSort(intArray);
+//        bubbleSort(intArray);
+        selectionSort(intArray);
 
 //        BUBBLE SORT
     }
